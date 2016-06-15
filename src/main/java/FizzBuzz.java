fea15fc9f9003d4eafbddf6af9a60c12cd048559
Krystian Ryniec <187796@edu.p.lodz.pl>
2016-06-15 16:44:59 +0200

    refactorization

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -2,7 +2,10 @@
  * Created by Krystian on 2016-06-15.
  */
 public class FizzBuzz {
+    private int number;
+
     public String getResult(int number) {
+        this.number = number;
         return "Fizz";
     }
 }
