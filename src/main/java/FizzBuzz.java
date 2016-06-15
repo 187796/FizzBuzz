3ab115e313bf25f62d346ab45f2053c040617b20
Krystian Ryniec <187796@edu.p.lodz.pl>
2016-06-15 16:54:37 +0200

    test "getResultShouldReturnTheSameNumberIfNoOtherRequirementIsFulfilled" green

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -9,6 +9,6 @@ public class FizzBuzz {
         if (number % 15 == 0) return "FizzBuzz";
         if (number % 3 == 0) return "Fizz";
         if (number % 5 == 0) return "Buzz";
-        return null;
+        return String.valueOf(number);
     }
 }
--- src/test/java/FizzBuzzTest.java
+++ src/test/java/FizzBuzzTest.java
@@ -33,8 +33,8 @@ public class FizzBuzzTest {
 
     @Test
     public void getResultShouldReturnTheSameNumberIfNoOtherRequirementIsFulfilled(){
-        assertEquals(1,fizzBuzz.getResult(1));
-        assertEquals(7,fizzBuzz.getResult(7));
-        assertEquals(13,fizzBuzz.getResult(13));
+        assertEquals("1",fizzBuzz.getResult(1));
+        assertEquals("7",fizzBuzz.getResult(7));
+        assertEquals("13",fizzBuzz.getResult(13));
     }
 }
